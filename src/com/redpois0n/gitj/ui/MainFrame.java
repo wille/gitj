20a55017f36785d21b0a5daee2f8a6dbb99f5713
redpois0n <red@cmail.nu>
2015-03-09 21:28:00 +0100

    changes..

--- src/com/redpois0n/gitj/ui/MainFrame.java
+++ src/com/redpois0n/gitj/ui/MainFrame.java
@@ -1,51 +1,62 @@
 package com.redpois0n.gitj.ui;
 
 import java.awt.BorderLayout;
-import java.util.List;
 
 import javax.swing.JFrame;
 import javax.swing.JMenuBar;
 import javax.swing.JPanel;
 import javax.swing.JSplitPane;
+import javax.swing.JTabbedPane;
 import javax.swing.border.EmptyBorder;
 
-import com.redpois0n.git.Commit;
 import com.redpois0n.git.Repository;
-import com.redpois0n.gitj.ui.components.JCommitPane;
 
 @SuppressWarnings("serial")
 public class MainFrame extends JFrame {
 
 	private JPanel contentPane;
-	private JSplitPane splitPaneMain;
-	private JCommitPane jcommitPane;
-	
+	private JTabbedPane tabbedPane;
+
 	public MainFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 800, 500);
+		setBounds(100, 100, 700, 500);
 		
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(0, 0, 0, 0));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(new BorderLayout(0, 0));
 		
-		splitPaneMain = new JSplitPane();
-		contentPane.add(splitPaneMain, BorderLayout.CENTER);
+		JSplitPane splitPane = new JSplitPane();
+		contentPane.add(splitPane, BorderLayout.CENTER);
 		
-		
+		tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		splitPane.setRightComponent(tabbedPane);
 	}
 
+	/**
+	 * Loads repository in new panel
+	 * @param repository
+	 */
 	public void loadRepository(Repository repository) {
-		try {
-			List<Commit> commits = repository.getCommits(true);
+		try {			
+			MainPanel pane = new MainPanel(repository);
 			
-			jcommitPane = new JCommitPane(commits);
-			
-			splitPaneMain.setRightComponent(jcommitPane);
+			addPanel(repository.getFolder().getName(), pane);
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
 	}
+	
+	/**
+	 * Add tab
+	 * @param title Tab title
+	 * @param panel
+	 */
+	public void addPanel(String title, MainPanel panel) {
+		tabbedPane.addTab(title, panel);
+	}
+	
+	
 }
