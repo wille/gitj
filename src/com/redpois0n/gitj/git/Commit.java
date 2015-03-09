10cb419841f121eae0d091f21d825c24ac4bacd7
redpois0n <red@cmail.nu>
2015-03-09 06:44:45 +0100

    Commit class

--- src/com/redpois0n/gitj/git/Commit.java
+++ src/com/redpois0n/gitj/git/Commit.java
@@ -5,6 +5,12 @@ import java.util.List;
 
 public class Commit {
 	
+	private Repository repo;
+	private String hash;
+	private String authorName;
+	private String authorEmail;
+	private String when;
+	private String comment;
 	private List<Diff> diffs = new ArrayList<Diff>();
 	
 	public Commit(Repository repo, String raw) {
@@ -16,7 +22,12 @@ public class Commit {
 	}
 	
 	public Commit(Repository repo, String hash, String authorName, String authorEmail, String when, String comment) {
-		
+		this.repo = repo;
+		this.hash = hash;
+		this.authorName = authorName;
+		this.authorEmail = authorEmail;
+		this.when = when;
+		this.comment = comment;
 	}
 	
 	public void addDiff(Diff diff) {
@@ -25,6 +36,30 @@ public class Commit {
 	
 	public List<Diff> getDiffs() {
 		return diffs;
+	}
+	
+	public Repository getRepository() {
+		return repo;
+	}
+
+	public String getHash() {
+		return hash;
+	}
+
+	public String getAuthorName() {
+		return authorName;
+	}
+
+	public String getAuthorEmail() {
+		return authorEmail;
+	}
+
+	public String getWhen() {
+		return when;
+	}
+
+	public String getComment() {
+		return comment;
 	}
 
 }
