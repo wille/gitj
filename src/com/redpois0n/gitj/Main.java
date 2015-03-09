e1e240e8bf233860120d4bb4b52413b273063854
redpois0n <red@cmail.nu>
2015-03-09 16:21:28 +0100

    MainFrame added

--- src/com/redpois0n/gitj/Main.java
+++ src/com/redpois0n/gitj/Main.java
@@ -2,12 +2,20 @@ package com.redpois0n.gitj;
 
 import java.io.File;
 
+import javax.swing.UIManager;
+
 import com.redpois0n.gitj.git.Repository;
+import com.redpois0n.gitj.ui.MainFrame;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception {
 		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+			
+			MainFrame frame = new MainFrame();
+			frame.setVisible(true);
+			
 			File dir = new File(".");
 			Repository repository = new Repository(dir);
 			repository.getCommits();
---
+++ src/com/redpois0n/gitj/ui/MainFrame.java
@@ -0,0 +1,23 @@
+package com.redpois0n.gitj.ui;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+@SuppressWarnings("serial")
+public class MainFrame extends JFrame {
+
+	private JPanel contentPane;
+
+	public MainFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
