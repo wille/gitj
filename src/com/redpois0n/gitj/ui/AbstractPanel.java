e1ca658214b56f3d543630e7296cefea3e03f983
redpois0n <red@cmail.nu>
2015-03-10 17:02:39 +0100

    Parent panel possible in AbstractPanel

--- src/com/redpois0n/gitj/ui/AbstractPanel.java
+++ src/com/redpois0n/gitj/ui/AbstractPanel.java
@@ -7,14 +7,23 @@ import com.redpois0n.git.Repository;
 @SuppressWarnings("serial")
 public abstract class AbstractPanel extends JPanel {
 	
+	protected AbstractPanel parent;
 	protected Repository repo;
 	
 	public AbstractPanel(Repository repo) {
 		this.repo = repo;
 	}
 	
+	public AbstractPanel(AbstractPanel parent) {
+		this.parent = parent;
+	}
+	
 	public Repository getRepository() {
-		return this.repo;
+		return this.parent == null ? this.repo : this.parent.getRepository();
+	}
+	
+	public AbstractPanel getParent() {
+		return this.parent == null ? this : this.parent;
 	}
 	
 	public void reload() throws Exception {
