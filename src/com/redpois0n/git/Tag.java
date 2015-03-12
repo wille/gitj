ff179c9637080f20289e71922586c1701a0145ce
redpois0n <red@cmail.nu>
2015-03-12 14:57:42 +0100

    Tag.Type

--- src/com/redpois0n/git/Tag.java
+++ src/com/redpois0n/git/Tag.java
@@ -2,23 +2,31 @@ package com.redpois0n.git;
 
 public class Tag {
 	
+	public static enum Type {
+		ANNOTATED, LIGHTWEIGHT;
+	}
+	
 	private String tag;
 	private String message;
 	private String tagger;
 	private String date;
+	private Type type;
 	
 	public Tag(String tag) {
 		this.tag = tag;
-		this.message = null;
+		this.type = Type.LIGHTWEIGHT;
 	}
 	
-	public Tag(String tag, String message) {
+	public Tag(String tag, String message, String tagger, String date) {
 		this.tag = tag;
 		this.message = message;
+		this.tagger = tagger;
+		this.date = date;
+		this.type = Type.ANNOTATED;
 	}
 	
 	public boolean isAnnotated() {
-		return this.message != null;
+		return this.type == Type.ANNOTATED;
 	}
 	
 	public String getTag() {
