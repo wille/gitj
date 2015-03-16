5a8cbf47de40421db686272f1771aa8402db2513
redpois0n <red@cmail.nu>
2015-03-17 00:15:04 +0100

    Correct layout

--- src/com/redpois0n/gitj/ui/CommitPanel.java
+++ src/com/redpois0n/gitj/ui/CommitPanel.java
@@ -1,16 +1,21 @@
 package com.redpois0n.gitj.ui;
 
 import com.redpois0n.git.Repository;
+import com.redpois0n.gitj.Main;
+
 import java.awt.BorderLayout;
+
 import javax.swing.JSplitPane;
 
 @SuppressWarnings("serial")
 public class CommitPanel extends AbstractPanel {
 	
+	private MainFrame parent;
 	private PanelUncommited panelList;
 
-	public CommitPanel(Repository repo) {
+	public CommitPanel(MainFrame parent, Repository repo) {
 		super(repo);
+		this.parent = parent;
 		setLayout(new BorderLayout(0, 0));
 		
 		JSplitPane splitPane = new JSplitPane();
@@ -22,13 +27,23 @@ public class CommitPanel extends AbstractPanel {
 		JSplitPane topSplitPane = new JSplitPane();
 		topSplitPane.setResizeWeight(0.5);
 		panelList = new PanelUncommited(repo);
-		topSplitPane.setRightComponent(panelList);
+		try {
+			panelList.reload();
+		} catch (Exception e) {
+			e.printStackTrace();
+			Main.displayError(e);
+		}
+		topSplitPane.setLeftComponent(panelList);
 		
 		splitPane.setLeftComponent(topSplitPane);
 	}
 	
 	public PanelUncommited getListPanel() {
 		return panelList;
+	}
+	
+	public void cancel() {
+		parent.removePanel(this);
 	}
 
 }
