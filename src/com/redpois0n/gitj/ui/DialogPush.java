b64a8fad20d075c3846a96773f1d454dc2c38bea
redpois0n <red@cmail.nu>
2015-03-20 08:31:33 +0100

    DialogPush, DialogRemotes

---
+++ src/com/redpois0n/gitj/ui/DialogPush.java
@@ -0,0 +1,13 @@
+package com.redpois0n.gitj.ui;
+
+import javax.swing.JDialog;
+
+@SuppressWarnings("serial")
+public class DialogPush extends JDialog {
+
+	public DialogPush() {
+		setBounds(100, 100, 450, 300);
+		
+	}
+
+}
---
+++ src/com/redpois0n/gitj/ui/DialogRemotes.java
@@ -0,0 +1,13 @@
+package com.redpois0n.gitj.ui;
+
+import javax.swing.JDialog;
+
+@SuppressWarnings("serial")
+public class DialogRemotes extends JDialog {
+
+	public DialogRemotes() {
+		setBounds(100, 100, 450, 300);
+		
+	}
+
+}
