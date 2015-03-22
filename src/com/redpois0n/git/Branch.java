c8bbf23570964cd23404942bd162bf744058675d
redpois0n <red@cmail.nu>
2015-03-22 18:37:25 +0100

    equals(), also field if it is selected (current branch)

--- src/com/redpois0n/git/Branch.java
+++ src/com/redpois0n/git/Branch.java
@@ -5,6 +5,7 @@ public class Branch {
 	private String name;
 	private Commit commit;
 	private String status;
+	private boolean isSelected;
 
 	public Branch(String name, Commit commit) {
 		this.name = name;
@@ -25,5 +26,24 @@ public class Branch {
 
 	public void setStatus(String status) {
 		this.status = status;
+	}
+	
+	public boolean isSelected() {
+		return isSelected;
+	}
+
+	public void setSelected(boolean isSelected) {
+		this.isSelected = isSelected;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (o instanceof Branch) {
+			Branch branch = (Branch) o;
+			
+			return branch.getCommit().equals(getCommit());
+		}
+		
+		return false;
 	}
 }
