dd6229c5edc25aece7cf33a67675243516facaa9
redpois0n <red@cmail.nu>
2015-03-22 02:06:17 +0100

    IOUtils

---
+++ src/com/redpois0n/gitj/utils/IOUtils.java
@@ -0,0 +1,15 @@
+package com.redpois0n.gitj.utils;
+
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+
+public class IOUtils {
+	
+	public static void setClipboard(String s) {
+		StringSelection selection = new StringSelection(s);
+	    Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+	    clipboard.setContents(selection, selection);
+	}
+
+}
