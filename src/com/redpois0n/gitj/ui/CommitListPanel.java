2d5310a24b56b81147bd988b5ffce37df341c647
redpois0n <red@cmail.nu>
2015-03-29 13:56:41 +0200

    Scrolls to tag

--- src/com/redpois0n/gitj/ui/CommitListPanel.java
+++ src/com/redpois0n/gitj/ui/CommitListPanel.java
@@ -5,6 +5,7 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
@@ -175,6 +176,7 @@ public class CommitListPanel extends JScrollPane {
 			
 			if (c == commit) {
 				table.setRowSelectionInterval(i, i);
+				table.scrollRectToVisible(new Rectangle(table.getCellRect(i, 0, true)));
 				break;
 			}
 		}
