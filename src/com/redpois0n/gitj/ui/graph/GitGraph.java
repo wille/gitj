e613f1173b7702ed94e8c46adc966203e0eab683
redpois0n <red@cmail.nu>
2015-07-01 23:59:38 +0200

    get graphentry from commit

--- src/com/redpois0n/gitj/ui/graph/GitGraph.java
+++ src/com/redpois0n/gitj/ui/graph/GitGraph.java
@@ -3,12 +3,24 @@ package com.redpois0n.gitj.ui.graph;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.redpois0n.git.Commit;
+
 public class GitGraph {
 	
 	private List<GraphEntry> list = new ArrayList<GraphEntry>();
 	
 	public void add(GraphEntry entry) {
 		this.list.add(entry);
+	}
+	
+	public GraphEntry get(Commit c) {
+		for (GraphEntry entry : list) {
+			if (entry.getCommit().equals(c)) {
+				return entry;
+			}
+		}
+		
+		return null;
 	}
 
 }
