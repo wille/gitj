790d76c25795bd4b8b2bf73c431258b1ce0cfb51
redpois0n <red@cmail.nu>
2015-07-01 23:55:03 +0200

    render functions

--- src/com/redpois0n/gitj/ui/graph/GraphEntry.java
+++ src/com/redpois0n/gitj/ui/graph/GraphEntry.java
@@ -1,5 +1,10 @@
 package com.redpois0n.gitj.ui.graph;
 
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import javax.swing.ImageIcon;
+
 import com.redpois0n.git.Commit;
 
 public class GraphEntry {
@@ -10,6 +15,22 @@ public class GraphEntry {
 	public GraphEntry(String graphData, Commit c) {
 		this.graphData = graphData;
 		this.commit = c;
+	}
+	
+	public BufferedImage render(int height) {
+		BufferedImage image = new BufferedImage(100, height, BufferedImage.TYPE_INT_RGB);
+		Graphics g = image.createGraphics();
+		
+		
+		return image;
+	}
+	
+	public ImageIcon renderIcon(int height) {
+		return new ImageIcon(render(height));
+	}
+	
+	public Commit getCommit() {
+		return this.commit;
 	}
 	
 }
