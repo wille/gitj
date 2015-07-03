484767363ee438dd6d1800775b7953cedc84532d
redpois0n <red@cmail.nu>
2015-07-03 02:17:22 +0200

    ANSI map

--- src/com/redpois0n/gitj/ui/graph/GitGraph.java
+++ src/com/redpois0n/gitj/ui/graph/GitGraph.java
@@ -1,12 +1,30 @@
 package com.redpois0n.gitj.ui.graph;
 
+import java.awt.Color;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.redpois0n.git.Commit;
 
 public class GitGraph {
 	
+	public static final Map<String, Color> COLORS = new HashMap<String, Color>();
+	
+	static {
+		COLORS.put("[30m", Color.black);
+		COLORS.put("[31m", Color.red);
+		COLORS.put("[32m", Color.green);
+		COLORS.put("[33m", Color.yellow);
+		COLORS.put("[34m", Color.blue);
+		COLORS.put("[35m", Color.magenta);
+		COLORS.put("[36m", Color.cyan);
+		COLORS.put("[37m", Color.white);
+	}
+	
+	public static final String ANSI_RESET = "\u001B[0m";
+
 	private List<GraphEntry> list = new ArrayList<GraphEntry>();
 	
 	public void add(GraphEntry entry) {
